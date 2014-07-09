89cdb0e741706b92ca3fcfd48d05a110a94e5efa
joebates25 <joebates25>
2014-07-08 23:26:04 -0500

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
 
+$(document).ready(function(){
+	$('.frontimage').slick({
+        dots: true,
+        infinite: true,
+        speed: 360,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5500
+        
+	});
+});
+
 
 var i = 0;
-
+/*
 function slideSwitch() {
     var $currentPhoto = $('#frontimage img.active');
     $currentPhoto.fadeOut("fast");
@@ -22,3 +36,4 @@ function slideSwitch() {
     $(function() {
         setInterval( "slideSwitch()", 5000 );
     });
+*/
