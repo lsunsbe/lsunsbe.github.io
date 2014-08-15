e4a18861a1a40090afd0ee3a53110a0a83bd053f
joebates25 <joebates25>
2014-08-15 16:13:50 -0500

    update

--- scripts/eboard.js
+++ scripts/eboard.js
@@ -1,6 +1,6 @@
 function setUnhidden(position) {
     "use strict"; 
-    var positions = ["president","vpresident","secretary","treasurer","progchair","commservchair","conplanchair","memchair","publication","socialchair","financechair","nsbesenate","procollinit","telecom","hist","senate","chairemer","parliamen"];
+    var positions = ["president","vpresident","secretary","treasurer","progchair","commservchair","conplanchair","memchair","publication","socialchair", "grad", "financechair","nsbesenate","procollinit","telecom","hist","senate","chairemer","parliamen"];
     var i = 0;
     for (; i < positions.length; i++){ 
         var currentPosition = document.getElementById(positions[i]);
@@ -8,8 +8,6 @@ function setUnhidden(position) {
             currentPosition.className = "visible";
         } else{
             currentPosition.className = "hidden";
-            if (positions[i] == "memchair"){
-            }
         }
         
         
