28d55af734006dbc689eeb27cd374978738895ec
joebates25 <joebates25>
2014-08-20 18:02:27 -0500

    update

--- scripts/index2.js
+++ scripts/index2.js
@@ -6,15 +6,20 @@
 
 $(document).ready(function(){    
     $('#gobutton').click(function( e ){
-        
+        $( "#gobutton" ).fadeOut(200);
+        var emailaddress = $('#emailbox').val;
+        $('#emailbox').val = "";
         $.ajax({
             type: "post",
             url: "emailsubmit.php",
             data: {e: $('#emailbox').val()},
             success: function(data){
                 window.alert(data);
+                $( "#gobutton" ).fadeIn(200);
+                $('#emailbox').val = emailaddress;
             }
-        })   
+        })
         e.preventDefault(); 
+       
     });
 });
