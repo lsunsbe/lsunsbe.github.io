b0a3e368149996a82c9f22fae301a3df17e1b0bf
joebates25 <joebates25>
2014-08-20 16:34:16 -0500

    f

---
+++ scripts/news.js
@@ -0,0 +1,3 @@
+function popup(url){
+    window.open("http://www.facebook.com/sharer.php?u=" + url, 'Share','height=400,width=675,left=10,top=10,resizable=no,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=yes');
+}
